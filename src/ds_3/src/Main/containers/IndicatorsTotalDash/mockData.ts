e809853623978c45fdd8fc3e50a10943cf47335f
antoivanov <antoivanov@korusconsulting.ru>
2023-02-16 19:43:31 +0300

    feat: add mockData for table (test purposes)

--- src/ds_3/src/Main/containers/IndicatorsTotalDash/mockData.ts
+++ src/ds_3/src/Main/containers/IndicatorsTotalDash/mockData.ts
@@ -1,14 +1,5 @@
 import { TChartLabelInfo } from 'src/Base/components/CustomChartLabel'
 
-export const options = [
-  { label: 'Все перспективы оценки' },
-  { label: 'Безопасность и надежность' },
-  { label: 'Клиенты и рынки' },
-  { label: 'Технологические процессы' },
-  { label: 'Экономика и финансы' },
-  { label: 'Персонал и развитие' }
-]
-
 export const labelSettings: TChartLabelInfo = {
   labelHeader: 'Всего отклонений:',
   labelData: 'XXX'
